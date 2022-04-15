fc32aac9ae5fa540fd78250385e9b09d7b33cb3b
celamyrrha <celamyrrha@gmail.com>
2022-04-15 18:32:28 -0300

    Desafio Dados de um usuário
    Em uma  academia estão cadastrando nome, idade e altura de vários usuários.
    
    Um determinado treino exige os seguintes requisitos: Ter 18 anos ou mais e ter uma altura igual ou maior a 1,70.
    
    Crie uma função chamada maiorAlto. Essa função irá receber um array como parâmetro. Este array terá o nome na primeira posição, a idade na segunda posição e a altura em centímetros na terceira posição. Ela também deve retornar verdadeiro (true) caso o aluno atenda os requisitos, ou falso (false) caso contrário.

---
+++ dados_usuario.js
@@ -0,0 +1,12 @@
+// Dados de um Usuário
+function maiorAlto(array) {
+    //Seu código vem aqui
+    var maior;
+    if ( array[1] >= 18 && array[2] >= 170){
+        maior = true;
+    }
+    else {
+        maior = false;
+    }
+    return maior;
+}   
